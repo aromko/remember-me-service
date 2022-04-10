7938685fd3d8875a094856220d22a7c582668c11
aromkoapps <aromkoapps@gmail.com>
2022-04-10 08:29:06 +0200

    format text message

--- pages/api/scheduledTasks/index.js
+++ pages/api/scheduledTasks/index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
         var yourDate = new Date().toISOString().split('T')[0];
     
         const reminders = await db.collection("Reminder").find({ executionAt: yourDate }).toArray();  
-        let textMessage = `Erinnerungen für ${yourDate}: %0D%0A `
+        let textMessage = `Erinnerungen für ${yourDate}: %0D%0A %0D%0A`
         reminders.forEach((item) => {
             textMessage += `<b>${item.name}</b> %0D%0A${item.description} %0D%0A%0D%0A`;            
         });
