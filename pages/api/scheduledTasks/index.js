a63b359f92910cadc9d7cdd44df92991e4d17fdb
aromkoapps <aromkoapps@gmail.com>
2022-07-10 08:49:14 +0200

    send reminders only if any are found for the day

--- pages/api/scheduledTasks/index.js
+++ pages/api/scheduledTasks/index.js
@@ -6,14 +6,16 @@ export default async function handler(req, res) {
     try {
         var yourDate = new Date().toISOString().split('T')[0];
     
-        const reminders = await db.collection("Reminder").find({ executionAt: yourDate }).toArray();  
-        let textMessage = `Erinnerungen für ${yourDate}: %0D%0A %0D%0A`
-        reminders.forEach((item) => {
-            textMessage += `<b>${item.name}</b> %0D%0A${item.description} %0D%0A%0D%0A`;            
-        });
-        fetch(
-            `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN}/sendMessage?parse_mode=html&chat_id=584840017&text=${textMessage}`
-        )
+        const reminders = await db.collection("Reminder").find({ executionAt: yourDate }).toArray();
+        if (reminders.length > 0) {
+            let textMessage = `Erinnerungen für ${yourDate}: %0D%0A %0D%0A`
+            reminders.forEach((item) => {
+                textMessage += `<b>${item.name}</b> %0D%0A${item.description} %0D%0A%0D%0A`;            
+            });
+            fetch(
+                `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN}/sendMessage?parse_mode=html&chat_id=584840017&text=${textMessage}`
+            )
+        }
         res.status(200).json('OK');
     } catch (e) {
         res.json({errorMessage: `Something went wrong. ${e.code}`});
