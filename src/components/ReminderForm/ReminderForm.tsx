036ee1e7c04ca3f5164ae83f83253b9ef85c5eee
aromko <aromkoapp@gmail.com>
2023-05-25 08:56:09 +0200

    refactor reminder form

--- src/components/ReminderForm/ReminderForm.tsx
+++ src/components/ReminderForm/ReminderForm.tsx
@@ -4,6 +4,7 @@ import { useResponse } from '../../customHooks';
 import theme from '@marigold/theme-b2b';
 import {
   Button,
+  Container,
   FieldGroup,
   Headline,
   MarigoldProvider,
@@ -11,18 +12,24 @@ import {
   Stack,
   TextField,
 } from '@marigold/components';
-import { Key } from 'react/index';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 
 interface User {
   _id: string;
   name: string;
 }
 
+interface IFormInputs {
+  name: string;
+  description: string;
+  executionAt: string;
+  messageType: string;
+  telegramId: string;
+}
+
 export const ReminderForm: React.FC = () => {
   const router = useRouter();
   const [users, setUsers] = useState<User[]>([]);
-  const [messageType, setMessageType] = useState('TELEGRAM');
-  const [telegramId, setTelegramId] = useState<string[]>([]);
   const message = useResponse();
 
   useEffect(() => {
@@ -33,116 +40,161 @@ export const ReminderForm: React.FC = () => {
       });
   }, []);
 
-  const saveReminder = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const saveReminder: SubmitHandler<IFormInputs> = data => {
+    const splitTelegramId = data.telegramId.split('_');
 
     fetch('/api/reminder', {
       method: 'POST',
       body: JSON.stringify({
-        // @ts-ignore
-        name: event.currentTarget['name'].value,
-        description: event.currentTarget['description'].value,
-        executionAt: event.currentTarget['executionAt'].value,
-        messageType: messageType,
-        userName: telegramId[1],
-        telegramId: telegramId[0],
+        name: data.name,
+        description: data.description,
+        executionAt: data.executionAt,
+        messageType: data.messageType,
+        telegramId: splitTelegramId[0],
+        userName: splitTelegramId[1],
       }),
     });
 
     router.push({
       pathname: '/',
       query: {
-        // @ts-ignore
-        message: `Reminder ${event.currentTarget['name'].value} successfully added.`,
+        message: `Reminder ${data.name} successfully added.`,
       },
     });
   };
 
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      name: '',
+      description: '',
+      executionAt: '',
+      messageType: 'TELEGRAM',
+      userName: '',
+      telegramId: '',
+    },
+  });
+
   return (
     <MarigoldProvider theme={theme}>
-      <Stack space="large" alignX="center">
-        <Stack space="xsmall">
+      <Container size="large" align="center">
+        <Stack space="medium">
           <FieldGroup labelWidth="large">
             <Headline level="2">Add Reminder</Headline>
-            <form onSubmit={saveReminder}>
-              <Stack space="medium">
-                <TextField
-                  id="name"
-                  name="name"
-                  label="Name:"
-                  required
-                  placeholder="Name"
-                  type="text"
-                  description="Please enter a name."
-                  onChange={message.resetResponse}
-                />
-                <TextField
-                  id="description"
-                  name="description"
-                  label="Description"
-                  required
-                  placeholder="Description"
-                  type="text"
-                  description="Please enter a description."
-                  onChange={message.resetResponse}
-                />
-                <TextField
-                  id="executionAt"
-                  name="executionAt"
-                  label="Execution date"
-                  required
-                  placeholder="Execution date"
-                  type="date"
-                  description="Please enter an execution date."
-                  onChange={message.resetResponse}
-                />
-                <Select
-                  id="messageType"
-                  name="messageType"
-                  required
-                  label="Message type"
-                  defaultSelectedKey="TELEGRAM"
-                  onChange={(selectedKey: Key) => {
-                    setMessageType(selectedKey as string);
-                  }}
-                >
-                  <Select.Option key={'TELEGRAM'}>Telegram</Select.Option>
-                </Select>
-                <Select
-                  id="telegramId"
-                  name="telegramId"
-                  required
-                  label="User"
-                  placeholder="Please select a user"
-                  onChange={(selectedKey: string | number | null) => {
-                    if (selectedKey !== null) {
-                      setTelegramId(String(selectedKey).split('_'));
-                    }
-                  }}
-                >
-                  {users.map(item => (
-                    <Select.Option
-                      key={`${item._id}_${item.name}`}
-                      // @ts-ignore
-                      value={`${item._id}_${item.name}`}
-                    >
-                      {item.name}
-                    </Select.Option>
-                  ))}
-                </Select>
-              </Stack>
-              <Stack alignX="right">
-                <Button variant="primary" size="small" type="submit">
-                  Save
-                </Button>
+            <form onSubmit={handleSubmit(saveReminder)}>
+              <Stack space="small">
+                <Stack space="medium">
+                  <Controller
+                    name="name"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Name:"
+                        required
+                        type="text"
+                        description="Please enter a name."
+                        error={field.value.length === 0}
+                        errorMessage="The field is required. Please enter a name."
+                      />
+                    )}
+                  />
+                  <Controller
+                    name="description"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Description"
+                        required
+                        type="text"
+                        description="Please enter a description."
+                        error={field.value.length === 0}
+                        errorMessage="The field is required. Please enter a description."
+                      />
+                    )}
+                  />
+                  <Controller
+                    name="executionAt"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Execution date"
+                        required
+                        type="date"
+                        description="Please enter an execution date."
+                        error={field.value.length === 0}
+                        errorMessage="The field is required. Please enter an execution date."
+                      />
+                    )}
+                  />
+                  <Controller
+                    name="messageType"
+                    control={control}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        required
+                        label="Message type"
+                        defaultSelectedKey="TELEGRAM"
+                      >
+                        <Select.Option key={'TELEGRAM'}>Telegram</Select.Option>
+                      </Select>
+                    )}
+                  />
+                  <Controller
+                    name="telegramId"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        id="telegramId"
+                        name="telegramId"
+                        required
+                        label="User"
+                        placeholder="Please select a user"
+                        error={field.value.length === 0}
+                        errorMessage="The field is required. Please choose a user."
+                      >
+                        {users.map(item => (
+                          <Select.Option
+                            key={`${item._id}_${item.name}`}
+                            textValue="test"
+                          >
+                            {item.name}
+                          </Select.Option>
+                        ))}
+                      </Select>
+                    )}
+                  />
+                </Stack>
+                <Stack alignX="right">
+                  <Button
+                    variant="primary"
+                    size="small"
+                    type="submit"
+                    disabled={!isValid}
+                  >
+                    Save
+                  </Button>
+                </Stack>
               </Stack>
             </form>
           </FieldGroup>
         </Stack>
-      </Stack>
-      {message.response.errorMessage.length > 0 ? (
-        <div style={{ color: 'red' }}>{message.response.errorMessage}</div>
-      ) : null}
+        {message.response.errorMessage.length > 0 ? (
+          <div style={{ color: 'red' }}>{message.response.errorMessage}</div>
+        ) : null}
+      </Container>
     </MarigoldProvider>
   );
 };
